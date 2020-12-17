e261ecbfc13fcccee82df655781d548ae45276c6
Riddhi Suteri <ridsuteri@gmail.com>
2020-12-17 18:43:03 +0530

    Complete news letter app

--- Newsletter-Signup/index.js
+++ Newsletter-Signup/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 const mailchimp = require('@mailchimp/mailchimp_marketing');
 
 const app = express();
@@ -15,22 +16,72 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
 
     console.log(req.body);
-    mailchimp.setConfig({
-        apiKey: process.env.API_KEY,
-        server: 'us7',
-    });
 
-    async function callPing() {
-        const response = await mailchimp.ping.get();
-        console.log(response);
+    var fname = req.body.fname;
+    var lname = req.body.lname;
+    var email = req.body.email;
+
+    var data = {
+        members: [
+            {
+                email_address: email,
+                statusCode: 'subscribed',
+                merge_fields: {
+                    FNAME: fname,
+                    LNAME: lname
+                }
+            }
+        ]
+    };
+
+    var jdata = JSON.stringify(data);
+
+    var options = {
+        url: `https://us7.api.mailchimp.com/3.0/lists/${process.env.AUDIENCE_ID}`,
+        method: 'POST',
+        headers: {
+            'Authorization': 'ridsuteri ' + process.env.API_KEY
+        },
+        body: jdata
     }
 
-    callPing();
+    request(options, function (error, response, body) {
+        if (error) {
+            console.log(error);
+            res.sendFile(__dirname + '/failure.html');
+        } else {
+            console.log(response.statusCode);
+            if (response.statusCode == '200') {
+                res.sendFile(__dirname + '/success.html');
+            }
+            else {
+                res.sendFile(__dirname + '/failure.html');
+            }
 
-    res.sendFile(__dirname + '/success.html');
+        }
+    });
+    // _________to check connection with mail chimp____________
+
+    // mailchimp.setConfig({
+    //     apiKey: process.env.API_KEY,
+    //     server: 'us7',
+    // });
+
+    // async function callPing() {
+    //     const response = await mailchimp.ping.get();
+    //     console.log(response);
+    // }
+
+    // callPing();
+
 });
 
-app.listen(3000, function (err) {
+// for routing the failure to home page
+app.post('/failure', function (req, res) {
+    res.redirect('/');
+});
+
+app.listen(process.env.PORT || 3000, function (err) {
     if (err) {
         console.log('Error Running Server');
         return;
